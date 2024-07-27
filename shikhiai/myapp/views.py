6e23046bfbd83a16b34de413e2dc2108c39f35b9
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-27 14:27:27 +0600

    first run app

--- shikhiai/myapp/views.py
+++ shikhiai/myapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(reqest):
     return HttpResponse("Hello Django!")
+
+def aishop(reqest):
+    return HttpResponse("Hello AI shop!")
