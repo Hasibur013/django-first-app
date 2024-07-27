25477af85ea31fb6b6dc9ffdf882765afa7c0a47
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-27 20:20:48 +0600

    add html

--- shikhiai/myapp/views.py
+++ shikhiai/myapp/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello Django!")
-    return render(request, 'index.html')
+    teacher = {'name': 'Hasibur', 'course': 'Django'}
+    return render(request, 'index.html', teacher)
 
 def aishop(request):
     return HttpResponse("Hello AI shop!")
