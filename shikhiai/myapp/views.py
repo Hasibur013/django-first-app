4ce7e3ea5b341e61b66b90ae9ea8a32c2a575d05
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-27 18:41:31 +0600

    templates

--- shikhiai/myapp/views.py
+++ shikhiai/myapp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(reqest):
-    return HttpResponse("Hello Django!")
+def home(request):
+    # return HttpResponse("Hello Django!")
+    return render(request, 'index.html')
 
-def aishop(reqest):
+def aishop(request):
     return HttpResponse("Hello AI shop!")
