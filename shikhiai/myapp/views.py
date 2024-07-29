f15014a9c6e679a2cd25558648d365904a1e6568
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-29 12:12:37 +0600

    added some templates

--- shikhiai/myapp/views.py
+++ shikhiai/myapp/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'home.html', {'current_page': 'home'})
+def home(request):
+    return render(request, 'home.html', {'current_page': 'home'})
+
+def blog(request):
+    return render(request, 'blog.html', {'current_page': 'blog'})
+
+def about(request):
+    return render(request, 'about.html', {'current_page': 'about'})
+
+def contact(request):
+    return render(request, 'contact.html', {'current_page': 'contact'})
