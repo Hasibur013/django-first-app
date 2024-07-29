44ab7da963b7b08114275d0435b1acbe6e404238
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-29 11:12:52 +0600

    delete basic concept and start new project

--- shikhiai/myapp/views.py
+++ shikhiai/myapp/views.py
@@ -2,10 +2,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    # return HttpResponse("Hello Django!")
-    teacher = {'name': 'Hasibur', 'course': 'Django'}
-    return render(request, 'index.html', teacher)
-
-def aishop(request):
-    return HttpResponse("Hello AI shop!")
