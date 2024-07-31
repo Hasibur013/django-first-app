747a009f76a8e95661ff055dc5c00902416a3dcb
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-31 08:38:38 +0600

    add postgresql

--- shikhiai/myapp/views.py
+++ shikhiai/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Contact
 
 # Create your views here.
 def dashboard(request):
@@ -14,4 +15,10 @@ def about(request):
     return render(request, 'about.html', {'current_page': 'about'})
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        desc = request.POST['desc']
+        values = Contact(name=name, email=email, desc=desc)
+        values.save()
     return render(request, 'contact.html', {'current_page': 'contact'})
